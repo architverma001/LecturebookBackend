a6f742806841d6b48ac054dc239dd696a23dc9ed
Archit verma <102369683+architverma001@users.noreply.github.com>
2023-06-09 17:09:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 const shortid = require('shortid');
 const Razorpay = require('razorpay');
@@ -14,9 +14,7 @@ app.post('/orders', orders)
 app.post('/verify', verify)
 
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "Hello World" })
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
